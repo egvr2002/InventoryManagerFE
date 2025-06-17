e5e863c095b642e4aea8a8db922d6a4a9470f507
Emmanuel Valentin <emmanuel.valentin@encora.com>
2025-06-17 11:25:58 -0600

    Add & set up shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+
 export default function App() {
   return (
-    <div className="flex min-h-dvh items-center justify-center bg-gradient-to-b from-white via-gray-200 to-gray-300">
+    <div className="flex min-h-dvh flex-col items-center justify-center gap-2 bg-gradient-to-b from-white via-gray-200 to-gray-300">
       <h1 className="text-2xl font-thin">Hello world!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
